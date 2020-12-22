2a73abdb4a229298f2aa04cf048298566ce0e2e7
wajahatAnwar <wajahat@ikarobar.com>
2020-12-22 20:45:23 +0500

    Added new page

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Storeks Privacy Policy</title>
+    <link
+  rel="stylesheet"
+  href="https://unpkg.com/@shopify/polaris@5.5.0/dist/styles.css"
+/>
 </head>
 <body>
     <p>Storeks Insurace Privacy Policy</p>
