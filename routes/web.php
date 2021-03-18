f6fa8601cae4c7cf3130a851a3c5c6c549bcc9d2
wajahatAnwar <wajahat@ikarobar.com>
2021-03-18 20:12:05 +0500

    Change in same site

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {return view('welcome');})->middleware(['auth.shopify', 'billable'])->name('home');
+Route::get('/', function () {
+    dd(Auth::check());
+    return view('welcome');
+})->middleware(['auth.shopify', 'billable'])->name('home');
 
 Route::middleware(['auth.shopify', 'billable'])->group(function () {
     Route::resource('products', 'ProductsController');
