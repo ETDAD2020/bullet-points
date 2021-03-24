0fd6a0982195f339c180d33455b522953d5a065e
wajahatAnwar <wajahat@ikarobar.com>
2021-03-24 13:49:08 +0500

    Changing referral logs

--- resources/views/layouts/referral.blade.php
+++ resources/views/layouts/referral.blade.php
@@ -38,9 +38,9 @@
       <!-- partial:partials/_navbar.html -->
       <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center" style="background-color: #16495c;">
-          <a class="navbar-brand brand-logo" href="index.html">
+          <a class="navbar-brand brand-logo" href="/">
             <img src="/assets/images/logo.png" alt="Sharealp Logo"> </a>
-          <a class="navbar-brand brand-logo-mini" href="index.html">
+          <a class="navbar-brand brand-logo-mini" href="/">
             <img src="assets/images/logo-mini.svg" alt="logo" /> </a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center">
